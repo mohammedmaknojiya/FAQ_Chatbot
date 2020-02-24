88e8a60cf8858be6324e0a600ce7612b14d4cccd
mohammedmaknojiya <42268865+mohammedmaknojiya@users.noreply.github.com>
2020-02-24 23:35:07 +0530

    all code uploaded

---
+++ chatbot_php/db.php
@@ -0,0 +1,21 @@
+<?php
+
+      $servername="localhost";
+      $username="root";
+      $password="";
+      $dbname="chatbot";
+
+      $server_time=date("Y-m-d H:i:s");
+
+      $conn=new mysqli($servername,$username,$password,$dbname);
+
+    //   if($conn){
+
+    //       echo "Connected";
+
+    //   }else{
+
+    //       echo "Failed to Connect";
+    // }
+
+?>
